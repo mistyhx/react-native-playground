d46a2e3a7653025ee3c93b987a9af39fdf664c67
xin hu <xinhu@xins-iMac.fios-router.home>
2018-07-29 22:33:09 -0400

    play around with basic  component

--- App.js
+++ App.js
@@ -1,35 +1,79 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Button, Alert} from 'react-native';
+import { ScrollView, FlatList } from 'react-native-gesture-handler';
+
+class PunchlistItem extends Component {
+  render(){
+    return(
+    <View style={styles.punchlist}>
+      <View style={styles.badge}>
+      <Text style={styles.titleText}>{this.props.category}</Text>
+      </View>
+
+
+       <Text>{this.props.description}</Text>
+     </View>
+    );
+  }
+}
+
 
 export default class App extends React.Component {
+
+  state = {
+
+         category:"A001",
+         description:"Tube broken need repair"
+    
+
+  }
   render() {
     return (
 
 
       <View style={styles.container}>
-        <View>
-            <Text style={styles.titleText}>A 001</Text>
-        </View>
+    
+          <PunchlistItem category={this.state.category} description={this.state.description}/>
+        
+        
+          <Button
+          onPress={() => {
+            Alert.alert('You successfully submitted');
+          }}
+          title="Submit"
+        />
 
 
-        <Text>Sharpen your knowledge about our capabilities </Text>
 
       </View>
     );
   }
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'flex-start',
-    justifyContent: 'center',
+ 
   },
 
     titleText: {
         fontSize: 20,
         fontWeight: 'bold',
+        color: 'white',
     },
+
+    badge: {
+      backgroundColor: 'red',
+      width: 60
+    },
+
+    punchlist: {
+      paddingLeft: 10,
+      marginTop:40,
+    }
 
 });
